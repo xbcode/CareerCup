79d874d1df77bbdefbaa6e4615f84230a963210d
littlemelon <xiaobao.code@gmail.com>
2013-04-05 18:14:44 -0400

    1. Implement algorithm by comparing if xy is substring xyxy;

---
+++ Ch1-ArraysAndStrings/src/com/littlemelon/craking/ch1/q8/RotateString.java
@@ -0,0 +1,26 @@
+package com.littlemelon.craking.ch1.q8;
+
+public class RotateString {
+	public static boolean isRotation(String s1,String s2){
+		int len=s1.length();
+
+		if(len==s2.length()&&len>0){
+			String s1s1=s1+s1;
+			return isSubString(s1s1,s2);
+		}
+		return false;
+	}
+	
+	public static boolean isSubString(String s1,String s2){
+		int len1=s1.length();
+		int len2=s2.length();
+		if(len1<len2){
+			return false;
+		}
+		for(int i=0;i<len1-len2+1;i++){
+			String s3=s1.substring(i, i+len2);
+			if(s3.equals(s2)) return true;
+		}
+		return false;	
+	}
+}
---
+++ Ch1-ArraysAndStrings/src/com/littlemelon/craking/ch1/q8/RotateStringTest.java
@@ -0,0 +1,26 @@
+package com.littlemelon.craking.ch1.q8;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class RotateStringTest {
+	
+	@Test
+	public void testIsRotation() {
+		Assert.assertTrue(RotateString.isRotation("abcd","bcda"));
+		Assert.assertFalse(RotateString.isRotation("asfe", "se"));
+		Assert.assertFalse(RotateString.isRotation("abcd","bcdaa"));
+	}
+
+	@Test
+	public void testIsSubString() {
+		Assert.assertTrue(RotateString.isSubString("abcd", "abcd"));
+		Assert.assertTrue(RotateString.isSubString("abcde", "abcd"));
+		Assert.assertTrue(RotateString.isSubString("abcd", ""));
+//		Assert.assertTrue(RotateString.isSubString("abcd", null));
+		Assert.assertFalse(RotateString.isSubString("abc", "abcd"));
+		Assert.assertFalse(RotateString.isSubString("abcd", "bcda"));
+	}
+
+}
