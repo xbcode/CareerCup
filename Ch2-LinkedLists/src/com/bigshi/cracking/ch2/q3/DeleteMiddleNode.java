0a1d86812f4541fd11d4802d970c4ae8f2812281
robert1024 <robert.shi.developer@gmail.com>
2013-04-09 10:54:29 -0400

    Add q3 package.

---
+++ Ch2-LinkedLists/src/com/bigshi/cracking/ch2/q3/DeleteMiddleNode.java
@@ -0,0 +1,13 @@
+package com.bigshi.cracking.ch2.q3;
+
+public class DeleteMiddleNode {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
